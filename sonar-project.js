172e28ce0873d25e9db4c2debb5564dc08fea0d8
MADHUHAAS NANNAKA <madhuhaasnannaka@gmail.com>
2020-01-23 06:11:05 +0000

    sonar-project.js created online with Bitbucket

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+     sonarqubeScanner({
+       serverUrl: 'https://sonarcloud.io',
+       options : {
+       'sonar.login':'418dafbb1bebe15e5d4c053e8bceb8cdfaaeb346'
+       'sonar.projectKey':'test-app'
+       'sonar.projectName':'test-app'
+       'sonar.projectVersion':'1.0'
+       'sonar.language':'js'
+       'sonar.sourceEncoding':'UTF-8'
+       'sonar.sources':'src'
+       'sonar.exclusions':'**/node_modules/**'
+       'sonar.tests':'src'
+       'sonar.test.inclusions':'**/*.spec.ts'
+       'sonar.typescript.lcov.reportPaths':'coverage/lcov.info'
+       'sonar.testExecutionReportPaths':'test-report.xml'
+       }
+     }, () => {});
