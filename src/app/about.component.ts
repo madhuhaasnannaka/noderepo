b570d82f1007f08c25b19555bc25d97f5f727b1e
MADHUHAAS NANNAKA <madhuhaasnannaka@gmail.com>
2020-01-24 07:14:20 +0000

    about.component.ts edited online with Bitbucket

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
   template: `
   <h2 highlight="skyblue">About</h2>
   <twain-quote></twain-quote>
-  <p>All about this sample</p>`
+  
 })
 export class AboutComponent { }
