f61df4d88842e3530bcf87ee9e8013e971cd344a
MADHUHAAS NANNAKA <madhuhaasnannaka@gmail.com>
2020-02-05 05:39:08 +0000

    sonar-project.js edited online with Bitbucket

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,22 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'https://sonarcloud.io',
-       token : "418dafbb1bebe15e5d4c053e8bceb8cdfaaeb346"
+       serverUrl: 'https://sonarcloud.io/',
+       token : '4ea57303c832f76242362c6cd9c9a73e63c27016',
        options : {
-       'sonar.organization=madhuhaasnannaka'
-       'Dsonar.projectKey=madhuhaasnannaka_cloudreponodejs'
-       'sonar.projectVersion':'1.0'
-       'sonar.language':'js'
-       'sonar.sourceEncoding':'UTF-8'
-       'sonar.sources':'src'
-       'sonar.exclusions':'**/node_modules/**'
-       'sonar.tests':'src'
-       'sonar.test.inclusions':'**/*.spec.ts'
-       'sonar.typescript.lcov.reportPaths':'coverage/lcov.info'
+       'sonar.login': '4ea57303c832f76242362c6cd9c9a73e63c27016',
+       'sonar.organization': 'lttsdevops',
+       'sonar.projectKey': 'lttsdevops_sample-nodejs-project',
+       'sonar.projectVersion': '1.0',
+       'sonar.language': 'js',
+       'sonar.sourceEncoding': 'UTF-8',
+       //'sonar.sources': '.',
+       'sonar.inclusions' : '**', // Entry point of your code
+       'sonar.exclusions': '*.test.*',
+       'sonar.tests': '.',
+       'sonar.test.inclusions': '**/testing/**,**/*.spec.ts',
+       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+       'sonar.testExecutionReportPaths': 'test-report.xml'
        
+
        }
      }, () => {});
